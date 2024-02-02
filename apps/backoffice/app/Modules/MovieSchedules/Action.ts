96b8db4fde82263a687f1c1a8c7743258a406158
egagofur <egagofurtriwahana@gmail.com>
2024-02-02 10:05:07 +0700

    style: rename file and change action type

--- apps/backoffice/app/Modules/MovieSchedules/Action.ts
+++ apps/backoffice/app/Modules/MovieSchedules/Action.ts
@@ -1,16 +1,16 @@
-import { Inertia, RequestPayload } from '@inertiajs/inertia';
+import { Inertia } from '@inertiajs/inertia';
 import { Route, route } from '../../Common/Route/Route';
 import { IMovieSchedule } from 'interface-models/movie/movie-schedule.interface';
 
-export const createMovieSchedule = (data: IMovieSchedule & RequestPayload) => {
-    Inertia.post(Route.MovieSchedulesCreate, data);
+export const createMovieSchedule = (data: IMovieSchedule) => {
+    Inertia.post(Route.MovieSchedulesCreate, data as unknown as FormData);
 };
 
-export const updateMovieSchedule = (
-    id: number,
-    data: IMovieSchedule & RequestPayload,
-) => {
-    Inertia.put(route(Route.MovieSchedulesEdit, { id }), data);
+export const updateMovieSchedule = (id: number, data: IMovieSchedule) => {
+    Inertia.put(
+        route(Route.MovieSchedulesEdit, { id }),
+        data as unknown as FormData,
+    );
 };
 
 export const deleteMovieSchedule = (id: number) => {
--- apps/backoffice/app/Modules/Movies/Action.ts
+++ apps/backoffice/app/Modules/Movies/Action.ts
@@ -1,17 +1,14 @@
-import { Inertia, RequestPayload } from '@inertiajs/inertia';
+import { Inertia } from '@inertiajs/inertia';
 import { Route, route } from '../../Common/Route/Route';
 import { IMovie } from 'interface-models/movie/movie.interface';
 
-export const updateMovie = async (
-    id: number,
-    data: IMovie & RequestPayload,
-) => {
-    Inertia.patch(route(Route.MovieEdit, { id }), data);
+export const updateMovie = async (id: number, data: IMovie) => {
+    Inertia.patch(route(Route.MovieEdit, { id }), data as unknown as FormData);
 };
 
-export const createMovie = (data: IMovie & RequestPayload) => {
+export const createMovie = (data: IMovie) => {
     new Promise((resolve, reject) => {
-        Inertia.post(Route.MovieCreate, data, {
+        Inertia.post(Route.MovieCreate, data as unknown as FormData, {
             onSuccess: (success) => {
                 resolve(success);
             },
